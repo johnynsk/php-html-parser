db04bae2b6d4b87269ae0bef431f2e79626cafd5
Evgeniy Vasilev <e.vasilev@office.ngs.ru>
2016-09-10 08:06:10 -0400

    garbage commit

---
+++ scripts/bootstrap.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * PSR-0 autoload implementation
+ *
+ * @param string $className
+ */
+spl_autoload_register(function ($className) {
+        $fileName = $className;
+
+        $fileName = preg_replace('/_/', '/', $fileName);
+        $fileName = preg_replace('/\\\\/', '/', $fileName);
+
+        $fileName = __DIR__ . DIRECTORY_SEPARATOR . $fileName . '.php';
+
+        if (file_exists($fileName)) {
+            return require $fileName;
+        }
+    }
+);
+
+
+if (!file_exists(dirname(__DIR__) . '/vendor/autoload.php')) {
+    throw new \Parser\Exception('Mismatch vendor/autoload.php. Run `composer install` before using it');
+}
+
+require_once dirname(__DIR__) . '/vendor/autoload.php';
+
--- www/page.php
+++ www/page.php
@@ -1,2 +1,12 @@
 <?php
-phpinfo();
+
+require_once dirname(__DIR__) . '/scripts/bootstrap.php';
+
+$parser = new \PHPHtmlParser\Dom();
+$contents = file_get_contents('http://johnynsk.ru/');
+$parser->load($contents);
+/** @var \PHPHtmlParser\Dom\HtmlNode[] $tags */
+$tags = $parser->find('a');
+var_dump($tags[0]->getAttributes());
+var_dump(array_keys((array)$parser->find('a')[0]));
+
